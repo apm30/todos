5bc73f6fddacdd99b547189866fe896caa9c1514
Domo <stolarczyk.dominik@gmail.com>
2020-01-10 09:06:27 +0100

    marking completed

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ class App extends Component {
 	state = {
 		todos: [
 			{ id: 1, title: 'pierwsza rzecz do zrobienia', completed: false },
-			{ id: 2, title: 'druga rzecz do zrobienia', completed: true },
+			{ id: 2, title: 'druga rzecz do zrobienia', completed: false },
 			{ id: 3, title: 'trzecia rzecz do zrobienia', completed: false }
 		]
+	};
+	markComplete = id => {
+		this.setState({
+			todos: this.state.todos.map(todo => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		});
 	};
 	render() {
 		console.log('todos :', this.state.todos);
 		return (
 			<div className="App">
-				<Todos todos={this.state.todos} />
+				<Todos todos={this.state.todos} markComplete={this.markComplete} />
 			</div>
 		);
 	}
