23958544d7fb576795caca0772c2e9e7e5cc0143
Domo <stolarczyk.dominik@gmail.com>
2020-01-10 07:51:08 +0100

    Todos component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
 	state = {};
 	render() {
-		return <h1>App</h1>;
+		return (
+			<div className="App">
+				<Todos />
+			</div>
+		);
 	}
 }
 
