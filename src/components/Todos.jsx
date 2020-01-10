95795c47c93cfe4872b1a8e4812847b5cb4470cb
Domo <stolarczyk.dominik@gmail.com>
2020-01-10 08:28:50 +0100

    prop types added

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
 	render() {
-		return this.props.todos.map(todo => <TodoItem todo={todo}></TodoItem>);
+		return this.props.todos.map(todo => (
+			<TodoItem key={todo.id} todo={todo}></TodoItem>
+		));
 	}
 }
+
+//prop types, good practice
+Todos.propTypes = {
+	todos: PropTypes.array.isRequired
+};
 
 export default Todos;
