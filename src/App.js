60485e8c0a8d10e6fef1111365f17f7de5213f19
Domo <stolarczyk.dominik@gmail.com>
2020-01-10 09:33:29 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
 
 class App extends Component {
 	state = {
@@ -29,6 +30,7 @@ class App extends Component {
 		console.log('todos :', this.state.todos);
 		return (
 			<div className="App">
+				<Header />
 				<Todos
 					todos={this.state.todos}
 					markComplete={this.markComplete}
